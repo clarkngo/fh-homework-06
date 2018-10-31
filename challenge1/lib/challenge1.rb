2acd36cf882b9f10b5444ea312d93bc406360c94
clarkngo <clarkngo@gmail.com>
2018-10-31 22:15:57 +0000

    finished ch01

--- challenge1/lib/challenge1.rb
+++ challenge1/lib/challenge1.rb
@@ -1,5 +1,34 @@
 class Challenge1
   def self.balanced?(_string)
     # your implementation goes here
+    openers_arr = []
+    brackets = { "{" => 1, "}" => 1, "[" => 2, "]" => 2, "(" => 3, ")" => 3 }
+
+    for i in 0.._string.length - 1
+
+      if _string[i] =~ /[\[{(]/
+        openers_arr.push(_string[i])
+      end
+
+      if _string[i] =~ /[\]})]/
+        closing = _string[i] 
+
+        if brackets[openers_arr.last] != brackets[closing]
+          false
+        else
+          openers_arr.pop
+        end
+
+      end
+
+    end
+
+    if openers_arr.empty?
+      true
+    else
+      false
+    end
+    
   end
+
 end
